524082d5894b2ea3d14fc32d6175a561df07830b
Mantvydas Liobė <mantvydasliobe@gmail.com>
2025-04-01 17:52:01 +0300

    inti

---
+++ inputSearcher.js
@@ -0,0 +1,6 @@
+const pBtn = document.getElementById("moreProffesional");
+const fBtn = document.getElementById("moreFunny");
+const lBtn = document.getElementById("lessWords");
+const mBtn = document.getElementById("moreWords");
+
+pBtn.addEventListener('click', () => console.log("more proffesional works"));
