5b874a44b6c4cb4673912f55bc68e96e7cceb9f5
Mantvydas Liobė <mantvydasliobe@gmail.com>
2025-04-01 18:35:26 +0300

    done in train

--- inputSearcher.js
+++ inputSearcher.js
@@ -3,4 +3,19 @@ const fBtn = document.getElementById("moreFunny");
 const lBtn = document.getElementById("lessWords");
 const mBtn = document.getElementById("moreWords");
 
-pBtn.addEventListener('click', () => console.log("more proffesional works"));
+const div = document.querySelector("div.devsite-searchbox");
+
+const changeText = () => {
+    if (div) {
+        const input = div.querySelector("input");
+        if (input) {
+            
+            input.value = "VEIKIA!!!"
+        }
+    }
+    console.log("siaip veikia");
+    console.log(div);
+
+}
+
+pBtn.addEventListener('click', () => changeText());
